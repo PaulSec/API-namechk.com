85aff666581cdb882a176c4ee7b97f514e9c92e8
PaulSec <aos.paul@gmail.com>
2015-10-15 11:43:44 +0100

    Updated the API for namechk

--- namechkAPI.py
+++ namechkAPI.py
@@ -4,13 +4,7 @@ Using this code, you can manage to retrieve info on a specific person with his n
 
 """
 import requests
-import re
-import urllib
-from hashlib import sha1
-from hmac import new as hmac
-import threading
-import Queue
-
+from bs4 import BeautifulSoup
 
 class NamechkAPI(object):
 
@@ -40,74 +34,23 @@ class NamechkAPI(object):
             print '[verbose] %s' % s
 
     def search(self, usernames):
-        url = 'http://namechk.com/Content/sites.min.js'
-        resp = requests.get(url)
-        # extract sites info from the js file
-        pattern = 'n:"([^"]+)",r:\d+,i:(\d+),s:"([^"]+)",b:"([^"]+)"'
-        sites = re.findall(pattern, resp.text.replace('\n', ''))
-        queue = Queue.Queue()
+        url = 'https://namechk.com/'
 
-        # starting threads
-        for i in range(5):
-            t = ThreadSearch(queue, self)
-            t.setDaemon(True)
-            t.start()
+        res = {}
+        s = requests.Session()
+        req = s.get(url)
+        token = req.cookies['token']
+        soup = BeautifulSoup(req.content, 'html.parser')
 
-        # inserting values in queue
+        providers = [provider['data-name'] for provider in soup.findAll('div', attrs={'class', 'record'})
+                    if "unavailable" not in provider['class']]
+
         for username in usernames:
-            result[username] = {}
-            for site in sites:
-                queue.put([username, site])
-
-        # waiting on queue
-        queue.join()
-        return result
-
-
-class ThreadSearch(threading.Thread):
-    def __init__(self, queue, obj):
-        threading.Thread.__init__(self)
-        self.queue = queue
-        self.obj = obj
-    
-    def run(self):
-        while True:
-            item = self.queue.get()
-            username, site = item[0], item[1]
-            # hardcoded key for hmac
-            key = '1Sx8srDg1u57Ei2wqX65ymPGXu0f7uAig13u'
-            # reset url for site requests
-            url = 'http://namechk.com/check'
-            # required header for site requests
-            headers = {'X-Requested-With': 'XMLHttpRequest'}
-
-            i = site[1]
-            name = site[0]
-            # build the hmac payload
-            message = "POST&%s?i=%s&u=%s" % (url, i, username)
-            b64_hmac_sha1 = '%s' % hmac(key, message, sha1).digest().encode('base64')[:-1]
-            payload = {'i': i, 'u': username, 'o_0': b64_hmac_sha1}
-            # build and send the request
-            try:
-                resp = requests.post(url, headers=headers, data=payload)
-                x = resp.text
-                if int(x) > 0:
-                    self.obj.display_message('%s: %s' % (name, STATUSES[x]))
-                    result[username][name] = STATUSES[x]
-                else:
-                    self.obj.display_message('%s: %s' % (name, 'Unknown error.'))
-            except KeyboardInterrupt:
-                raise KeyboardInterrupt
-            except Exception as e:
-                self.obj.display_message('%s: %s' % (name, e.__str__()))
-                pass
-            self.queue.task_done()
-
-STATUSES = {
-    '1': 'Available',
-    '2': 'User Exists!',
-    '3': 'Unknown',
-    '4': 'Indefinite'
-}
-
-result = {}
+            res[username] = []
+            for provider in providers:
+                tmp_url = "{0}/availability/{1}?q={2}&x={3}".format(url, provider, username, token)
+                data = s.get(tmp_url).json()
+                if (not data['available']):
+                    self.display_message("{0} exists on {1}".format(username, provider))
+                    res[username].append(provider)
+        return res
